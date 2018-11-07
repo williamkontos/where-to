aed76962a6a1a52b6045fd6f1e85f7bcee852e4a
William Kontos <williamjkontos@gmail.com>
2018-11-07 17:54:31 -0500

    created restaurant and dish models | migrated db

---
+++ db/migrate/20181107224551_create_dishes.rb
@@ -0,0 +1,10 @@
+class CreateDishes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dishes do |t|
+      t.string :name
+      t.decimal :price, :precision => 8, :scale => 2
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_221524) do
+ActiveRecord::Schema.define(version: 2018_11_07_224551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
